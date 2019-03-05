3840811dc594fea391baa0659a80c85c4037e4f5
chenguandong <chenguandong@limijiaoyin.com>
2019-03-05 14:33:02 +0800

    新增Apple rss  播客 主界面  列表圆角

--- lib/main.dart
+++ lib/main.dart
@@ -36,14 +36,5 @@ class MyApp extends StatelessWidget {
 
   }
 
-
-
-/*  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'ListView',
-      home: new MyHomePage(),
-    );
-  }*/
 }
 
